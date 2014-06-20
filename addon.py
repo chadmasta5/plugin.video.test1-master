8102364b45ee757f25c02ac517a658100007f0e4
chadmasta5 <chadmasta5@gmail.com>
2014-06-20 03:32:21 -0500

    Create addon.py

---
+++ addon.py
@@ -0,0 +1,36 @@
+import sys
+import urllib
+import urlparse
+import xbmcgui
+import xbmcplugin
+
+base_url = sys.argv[0]
+addon_handle = int(sys.argv[1])
+args = urlparse.parse_qs(sys.argv[2][1:])
+
+xbmcplugin.setContent(addon_handle, 'movies')
+
+def build_url(query):
+    return base_url + '?' + urllib.urlencode(query)
+
+mode = args.get('mode', None)
+
+if mode is None:
+    url = build_url({'mode': 'folder', 'foldername': 'test1'})
+    li = xbmcgui.ListItem('asdf', iconImage='DefaultFolder.png')
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
+                                listitem=li, isFolder=True)
+
+    url = build_url({'mode': 'folder', 'foldername': 'test2'})
+    li = xbmcgui.ListItem('assdf', iconImage='DefaultFolder.png')
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
+                                listitem=li, isFolder=True)
+
+    xbmcplugin.endOfDirectory(addon_handle)
+
+elif mode[0] == 'folder':
+    foldername = args['foldername'][0]
+    url = 'http://w325307.blob2.ge.tt/streams/9kGjGDl1/tutorial.mp4'
+    li = xbmcgui.ListItem(foldername + ' Video', iconImage='DefaultVideo.png')
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+    xbmcplugin.endOfDirectory(addon_handle)
